e9baa91376a960f18db05d0cf25a09361210a615
Arushi Renschler Pandey <arushirenschlerpandey@Arushis-MacBook-Air.local>
2017-12-01 12:31:18 -0500

    Added First Pages

---
+++ HitchHike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitchHike
+//
+//  Created by Arushi Renschler Pandey on 12/1/17.
+//  Copyright © 2017 Arushi Renschler Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
